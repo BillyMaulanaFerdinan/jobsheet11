59e591681688a0dc4fb5ad8b493b187ba9d97df4
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2023-11-09 14:21:05 +0700

    jobs 11

---
+++ Quiz.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Quiz {
+
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+        
+        char menu = 'y';
+        do{
+            
+            int number = random.nextInt(10)+1;
+            boolean success = false;
+            do{
+                System.out.print("Tebak angka(1-10): ");
+                int answer = input.nextInt();
+                input.nextLine();
+                success = (answer == number);
+                if (answer<number) {
+                    System.out.println("Jawaban anda lebih kecil!");
+                }else if (answer>number) {
+                    System.out.println("Jawaban anda lebih besar!");
+                }else if (success) {
+                    System.out.println("Jawaban anda benar!");
+                }
+        }while(!success);
+        System.out.print("Apakah Anda ingin Mengulang Permainan (Y/N)?");
+        menu = input.nextLine().charAt(0);
+    }while(menu=='y'||menu=='Y');
+    if (menu == 'n'||menu=='N') {
+        System.out.println("Terima kasih sudah bermain");
+    }else
+    System.out.println("Input salah");
+    boolean success = false;
+}
+}
